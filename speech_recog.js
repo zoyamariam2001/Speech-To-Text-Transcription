c35be9ea29b6e81ed53ed5db9e02725944a7286f
Zoya Mariam Patel <96236773+zoyamariam2001@users.noreply.github.com>
2023-06-17 01:39:52 +0530

    Update speech_recog.js

--- speech_recog.js
+++ speech_recog.js
@@ -1,4 +1,5 @@
 
+
 if ("webkitSpeechRecognition" in window) {
 
     var speechRecognition = new webkitSpeechRecognition();
@@ -10,14 +11,17 @@ if ("webkitSpeechRecognition" in window) {
   
     speechRecognition.onstart = () => {
       document.querySelector("#status").style.display = "block";
+      document.querySelector("#status2").style.display = "block";
     };
     speechRecognition.onerror = () => {
       document.querySelector("#status").style.display = "none";
+      document.querySelector("#status2").style.display = "none";
       console.log("Speech Recognition Error");
     };
     speechRecognition.onend = () => {
       document.querySelector("#status").style.display = "none";
-      console.log("Speech Recognition Ended");
+      document.querySelector("#status2").style.display = "none";
+      //console.log("Speech Recognition Ended");
     };
   
     speechRecognition.onresult = (event) => {
@@ -25,9 +29,25 @@ if ("webkitSpeechRecognition" in window) {
   
       for (let i = event.resultIndex; i < event.results.length; ++i) {
         if (event.results[i].isFinal) {
-          final_transcript += event.results[i][0].transcript;
-          final_transcript+=" ";
-        } else {
+
+          if (navigator.userAgent.indexOf("Chrome") !=-1 ){
+            var event_res=(event.results[i][0].transcript);
+            if(final_transcript==""){
+            event_res=event_res.charAt(0).toUpperCase()+event_res.slice(1)+".";
+            }
+            else{
+                event_res=" "+ event_res.charAt(1).toUpperCase()+event_res.slice(2)+".";
+            }
+            final_transcript += event_res;
+          }
+
+          else{                                                          //for browsers other than Google             
+            final_transcript += event.results[i][0].transcript;
+            final_transcript+=" ";
+          }
+        }
+        
+        else {
           interim_transcript += event.results[i][0].transcript;
         }
       }
